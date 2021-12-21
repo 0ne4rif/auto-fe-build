c77466ae6bedbd73be2771c62a95b619ed511cda
Wan Arif <54633167+0ne4rif@users.noreply.github.com>
2021-12-21 15:19:00 +0800

    Add files via upload

---
+++ fe_build.bat
@@ -0,0 +1,11 @@
+SET "WorkingDir=C:\Users\{YOUR_PC_USER}"
+SET "BaseURL=https://{REPO_SITE}"
+SET "BranchName=main"
+
+cd "%WorkingDir%\Documents"
+git clone "%BaseURL%/{REPO}/_git/{REPO}"
+cd "{REPO}\{REPO}\ClientApp"
+git checkout "%BranchName%"
+npm install
+npm run build
+pause
